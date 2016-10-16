1a43f6e546980ddab446c9f4fae382654d6e7acc
vmccreary <victoria.mccreary@gmail.com>
2016-10-16 08:35:18 -0500

    hover boards are out, hover text is in

---
+++ SpookyGameJam/Assets/Scripts/HoverText.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HoverText : MonoBehaviour {
+
+    [SerializeField]
+    GameObject ingredient;
+    [SerializeField]
+    Text itemText;
+    [SerializeField]
+    string hoverText;
+
+    private float height, width;
+    private float textPos;
+    private string currText;
+
+    // Use this for initialization
+    void Start () {
+        height = ingredient.transform.position.y;
+        width = ingredient.transform.position.x;
+        textPos = height + 0.65f;
+        currText = "";
+        itemText.rectTransform.position = new Vector2(width, textPos);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        itemText.text = currText;
+	}
+
+    void OnMouseEnter()
+    {
+        height = ingredient.transform.position.y;
+        width = ingredient.transform.position.x;
+        currText = hoverText;
+    }
+
+    void OnMouseExit()
+    {
+        currText = "";
+    }
+}
