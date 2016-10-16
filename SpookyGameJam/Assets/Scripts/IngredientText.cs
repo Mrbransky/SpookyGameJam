57e94fc36407231f33075190a419ff09cccd1fa5
vmccreary <victoria.mccreary@gmail.com>
2016-10-16 15:13:41 -0500

    room 1 done

--- SpookyGameJam/Assets/Scripts/IngredientText.cs
+++ SpookyGameJam/Assets/Scripts/IngredientText.cs
@@ -23,7 +23,7 @@ public class IngredientText : MonoBehaviour
 
     IEnumerator Wait()
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(4.0f);
         TurnOffMessage();
     }
 
